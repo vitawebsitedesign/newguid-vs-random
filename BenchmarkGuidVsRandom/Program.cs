570a3738a5af271af2998698c8ce2ec771e07cc3
Michael Nguyen <mn@3dsafety.com.au>
2019-09-16 14:26:19 +1000

    initial commit

---
+++ BenchmarkGuidVsRandom/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BenchmarkGuidVsRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var laps = 10;
+            var runs = 500000;
+            var sw = Stopwatch.StartNew();
+
+            for (var lap = 0; lap < laps; lap++)
+            {
+                for (var i = 0; i < runs; i++)
+                {
+                    Guid.NewGuid();
+                }
+                Console.WriteLine($"Guid.NewGuid took {sw.ElapsedMilliseconds} ms");
+                sw.Restart();
+            }
+
+            for (var lap = 0; lap < laps; lap++)
+            {
+                for (var i = 0; i < runs; i++)
+                {
+                    new Random().Next(0, 99).ToString("D2");
+                }
+                Console.WriteLine($"Random.Next took {sw.ElapsedMilliseconds} ms");
+
+            }
+
+            sw.Stop();
+            Console.ReadKey();
+        }
+    }
+}
